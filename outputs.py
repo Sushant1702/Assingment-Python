f6b8cea7f1962d29e8d84f886cf4ca9bcacf4df3
Sushant1702 <97727281+Sushant1702@users.noreply.github.com>
2022-01-20 23:58:34 +0530

    Add files via upload

---
+++ outputs.py
@@ -0,0 +1,36 @@
+First Question
+Enter first number 10
+Enter second number 20
+Enter third number 30
+The average of three numbers is :
+20.0
+
+
+Second Question
+Enter the gross income 100000
+Enter the number of dependents 5
+Your income tax is :
+17399.0
+
+
+Third Question
+Enter the SID 21103117
+Enter the name Sushant 
+Enter your Gender Male
+Enter your course name CSE
+Enter your CGPA 9
+[' 21103117', ' Sushant', ' Male', ' CSE', 9.0]
+
+
+Fourth Question
+Enter marks of students 95
+Enter marks of students 94
+Enter marks of students 92
+Enter marks of students 93
+Enter marks of students 91
+[' 91', ' 92', ' 93', ' 94', ' 95']
+
+
+Fifth Question
+Part a question :  ['Red', 'Green', 'White', 'Pink', 'Yellow']
+Part b of question : ['Red', 'Green', 'White', 'Purple', 'Yellow']
